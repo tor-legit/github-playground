6f2d2bf9dd6edae39ee7816e4cd73ca39f9df51d
torbeerandom <tor@legitsecurity.com>
2022-02-07 16:52:57 +0200

    tar test works

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -5,17 +5,21 @@ function buildAttack (n) {
   }
   return ret
 }
+const tar = require('tar')
 const tmpl = require('tmpl')
 const globParent = require('glob-parent')
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
-    for (let i = 1; i <= 50000; i++) {
+    for (let i = 1; i <= 1; i++) {
       const time = Date.now()
       const attackStr = '' + '{'.repeat(i * 10000) + 'answer'
       tmpl(attackStr, { answer: 42 })
       const timeCost = Date.now() - time
       console.log('attackStr.length: ' + attackStr.length + ': ' + timeCost + ' ms')
     }
+    tar.x({
+      file: 'my-tarball.tar.gz'
+    }).then(_ => {})
     globParent(buildAttack(5000))
     const data = 'data'
     expect(data).toBe('data')
