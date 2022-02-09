cc66f09165ffc01c5eb98d8efaa06db4b58f2920
torbeerandom <tor@legitsecurity.com>
2022-02-09 16:46:31 +0200

    test tar maketar

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,15 +1,35 @@
-function buildAttack (n) {
-  let ret = '{'
-  for (let i = 0; i < n; i++) {
-    ret += '/'
-  }
-  return ret
-}
+const makeTar = require('../lib/make-tar.js')
+const path = require('path')
+const mkdirp = require('mkdirp')
+const fixtures = path.resolve(__dirname, '../lib/fixtures')
+const unpackdir = path.resolve(fixtures, 'unpack')
+
 const tmpl = require('tmpl')
-const globParent = require('glob-parent')
-const tar = require('tar')
+
 describe('App', () => {
   it('Contains the compiled JavaScript', async () => {
+    const dir = path.resolve(unpackdir, 'dir-cache-error')
+    mkdirp.sync(dir + '/sync/y')
+    mkdirp.sync(dir + '/async/y')
+    makeTar([
+      {
+        path: 'x',
+        type: 'Directory'
+      },
+      {
+        path: 'x',
+        type: 'SymbolicLink',
+        linkpath: './y'
+      },
+      {
+        path: 'x/ginkoid',
+        type: 'File',
+        size: 'ginkoid'.length
+      },
+      'ginkoid',
+      '',
+      ''
+    ])
     for (let i = 1; i <= 1; i++) {
       const time = Date.now()
       const attackStr = '' + '{'.repeat(i * 10000) + 'answer'
@@ -17,10 +37,6 @@ describe('App', () => {
       const timeCost = Date.now() - time
       console.log('attackStr.length: ' + attackStr.length + ': ' + timeCost + ' ms')
     }
-    tar.x({
-      file: 'my-tarball.tar.gz'
-    }).then(_ => {})
-    globParent(buildAttack(5000))
     const data = 'data'
     expect(data).toBe('data')
   })
